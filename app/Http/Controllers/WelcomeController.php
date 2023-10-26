fbf58516523b6445e9577c56746bea2f4eea00db
Ryan DM <ryan.m.de.man@gmail.com>
2023-10-26 22:44:04 +0200

    welcome update

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Games;
+use App\Models\Tag;
 
 class WelcomeController extends Controller
 {
     public function welcome()
     {
         $games = Games::all(); 
+        $tags = Tag::all();
 
-        return view('welcome', compact('games'));
+        return view('welcome', compact('games','tags'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,8 +13,8 @@
         <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css"/>
 
     </head>
-    <body class="antialiased">
-        <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+ 
+        <div class="relative sm:flex sm:justify-center sm:items-center py-10 bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
                     @auth
@@ -28,6 +28,8 @@
                     @endauth
                 </div>
             @endif
+         </div>
+         
             <div class="container mx-auto p-6">
     <h1 class="text-3xl font-semibold text-gray-900 dark:text-white">Our Products</h1>
     <!-- Product Cards -->
@@ -38,6 +40,13 @@
             <h2 class="text-xl font-semibold text-gray-900 dark:text-white mt-2">{{ $game->name }}</h2>
             <p class="text-gray-500 dark:text-gray-400 mt-2">{{ $game->id }}</p>
             <p class="text-gray-700 dark:text-gray-500 mt-2">{{ $game->description }}</p>
+             @if ($game->tags->count() > 0)
+            <p class="text-gray-500 dark:text-gray-400 mt-2">
+                Tags: {{ $game->tags->pluck('name')->implode(', ') }}
+            </p>
+        @else
+            <p class="text-gray-500 dark:text-gray-400 mt-2">No tags available</p>
+        @endif
             @if ($game->creator)
                     <p class="text-gray-500 dark:text-gray-400 mt-2">Created by: {{ $game->creator->name }}</p>
                 @else
@@ -49,6 +58,6 @@
     </div>
 </div>
     </div>
-    </body>
+
     
 </html>
